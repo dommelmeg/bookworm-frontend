61eb74f9273176e1bf0b31e84cc16e8ab3af8fb4
Megan <dommelmeg@gmail.com>
2023-04-03 23:48:53 -0400

    fixes review stars & adds beginning of delete book

--- src/components/BookCards.js
+++ src/components/BookCards.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, CardBody, Box, Stack, Text, Heading, Image, CardFooter } from '@chakra-ui/react'
+import { Card, CardBody, Box, Stack, Text, Heading, Image, CardFooter, Flex } from '@chakra-ui/react'
 import BookDetailModal from "./BookDetailModal";
 import { StarIcon } from '@chakra-ui/icons'
 
@@ -26,13 +26,20 @@ const BookCards = ({ book }) => {
             <Image
               src={book.done_reading ? book.image : null}
             />
-            <Heading size='sm'>{book.title}</Heading>
+            <Heading paddingTop={book.done_reading? 3 : 0} size='sm'>{book.title}</Heading>
             <Text size='sm'>{book.author}</Text>
           </Box>
         </CardBody>
         <CardFooter>
           <Stack>
-            <Text><b>{avgRating} <StarIcon color='yellow.500' /></b></Text>
+            <Flex>
+              {!avgRating && <em>No Reviews Yet</em>}
+              {avgRating > 0 && <StarIcon color='yellow.500' />}
+              {avgRating > 1 && <StarIcon color='yellow.500' />}
+              {avgRating > 2 && <StarIcon color='yellow.500' />}
+              {avgRating > 3 && <StarIcon color='yellow.500' />}
+              {avgRating > 4 && <StarIcon color='yellow.500' />}
+            </Flex>
             <BookDetailModal book={book} />
           </Stack>
         </CardFooter>
--- src/components/BookDetailModal.js
+++ src/components/BookDetailModal.js
@@ -1,4 +1,5 @@
 import React from "react";
+import CommentCard from "./CommentCard";
 import { 
   Box,
   Modal,
@@ -14,10 +15,10 @@ import {
   ModalCloseButton, 
   Button, 
   useDisclosure, 
-  GridItem 
+  GridItem,
+  Flex
 } from '@chakra-ui/react'
-import CommentCard from "./CommentCard";
-import { StarIcon } from '@chakra-ui/icons'
+import { StarIcon, DeleteIcon } from '@chakra-ui/icons'
 
 
 
@@ -32,9 +33,25 @@ const BookDetailModal = ({ book }) => {
   )
   const avgRating = Math.floor(sum/reviews.length)
 
+  const handleDoneReadingClick = () => {
+    console.log(book.done_reading)
+  }
+
+  const handleDeleteButton = () => {
+    fetch(`http://localhost:9292/books/${book.id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+    .then((r) => r.json())
+    //FIX THIS FILTER OUT THIS BOOK & UPDATE STATE & USE CONTEXT YA DUMMY
+    .then((deletedBook) => console.log(deletedBook));
+  }
+
   return (
     <>
-      <Button mt={4} onClick={onOpen}>
+      <Button mt={4} onClick={onOpen} size='sm' >
         More Details
       </Button>
       <Modal finalFocusRef={finalRef} isOpen={isOpen} onClose={onClose}>
@@ -59,38 +76,46 @@ const BookDetailModal = ({ book }) => {
                   <Text fontSize='xs'>
                     Author
                   </Text>
-                  <Text fontSize='lg' >
+                  <Text fontSize='lg'>
                     {book.author}
                   </Text>
                 </Box>
                 
-                <Divider />
-
-                <Box>
+                <Box paddingTop={3}>
                   <Text fontSize='xs'>
                     Genre
                   </Text>
                   <Text fontSize='lg'>
                     {book.genre}
                   </Text>
+                  {book.done_reading ? null : <Button marginTop={4} onClick={handleDoneReadingClick}>Done Reading!</Button>}
                 </Box>
               </GridItem>
 
               <GridItem colSpan={3} rowSpan={1}>
-                <Text fontSize='xs'>
-                  Average Rating: {avgRating} <StarIcon color='yellow.500' />
+                <Text fontSize='xl'><b>Comments</b> 
+                  <Button size='sm' variant='ghost' onClick={onOpen}> + </Button>
                 </Text>
+                <Flex>
+                  {!avgRating && <em>No Reviews Yet</em>}
+                  {avgRating > 0 && <StarIcon color='yellow.500' />}
+                  {avgRating > 1 && <StarIcon color='yellow.500' />}
+                  {avgRating > 2 && <StarIcon color='yellow.500' />}
+                  {avgRating > 3 && <StarIcon color='yellow.500' />}
+                  {avgRating > 4 && <StarIcon color='yellow.500' />}
+                </Flex>
+                <Divider paddingTop={2} />
                 <CommentCard reviews={reviews} />
               </GridItem>
             </Grid>
           </ModalBody>
 
           <ModalFooter>
-            <Button variant='ghost'>Edit</Button>
-            <Button variant='ghost'>Delete Book</Button>
-            <Button colorScheme='blue' mr={3} onClick={onClose}>
+            {/* <Button variant='ghost'>Edit Book</Button> */}
+            <Button colorScheme='teal' mr={3} onClick={onClose}>
               Close
             </Button>
+            <Button margin={2} variant='ghost' onClick={handleDeleteButton} ><DeleteIcon /></Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
